081b29fc257d6a8b8f1e8d9e80dea254c37fc4df
hamidreza ghahremani <97837767+hamidreza01@users.noreply.github.com>
2023-04-17 19:51:17 +0330

    Implementation of Decision Trees algorithm
    The implementation defines a `DecisionTree` struct that has a `root` field, which represents the root of the decision tree. The `node` struct represents a single node in the tree and has fields for the feature that the node splits on (`splitFeature`), the value of the feature that the node splits on (`splitValue`), and the left and right child nodes (`left` and `right`).
    
    The `DataPoint` struct represents a single data point in the input data, and has fields for the features and the label.
    
    The `NewDecisionTree` function takes a slice of `DataPoint` structs and returns a new `DecisionTree` struct that has been trained on the input data.
    
    The `classify` method takes a slice of feature values and returns the predicted label for the input data based on the trained decision tree.
    
    The `buildTree` function is a helper function that recursively builds the decision tree by splitting the input data based on the feature that maximizes the information gain. The `traverseTree` function is a helper function that recursively traverses the decision tree to classify input data.
    
    The `computeInfoGain` function calculates the information gain of splitting the input data on a given feature, and the `computeEntropy` function calculates the entropy of a set of data points.
    
    The `computeSplitValue` function calculates the average value of a feature across a set of data points.

---
+++ dt.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type DecisionTree struct {
+	root *node
+}
+
+type node struct {
+	splitFeature int
+	splitValue   float64
+	left         *node
+	right        *node
+	label        int
+}
+
+type DataPoint struct {
+	features []float64
+	label    int
+}
+
+func NewDecisionTree(dataPoints []DataPoint) *DecisionTree {
+	root := buildTree(dataPoints, 0)
+	return &DecisionTree{root: root}
+}
+
+func (dt *DecisionTree) classify(features []float64) int {
+	return traverseTree(features, dt.root)
+}
+
+func buildTree(dataPoints []DataPoint, depth int) *node {
+	if len(dataPoints) == 0 {
+		return &node{label: -1}
+	}
+
+	label := dataPoints[0].label
+	sameLabel := true
+	for _, dp := range dataPoints {
+		if dp.label != label {
+			sameLabel = false
+			break
+		}
+	}
+	if sameLabel {
+		return &node{label: label}
+	}
+
+	splitFeature := -1
+	maxInfoGain := -math.MaxFloat64
+	for i := 0; i < len(dataPoints[0].features); i++ {
+		infoGain := computeInfoGain(dataPoints, i)
+		if infoGain > maxInfoGain {
+			maxInfoGain = infoGain
+			splitFeature = i
+		}
+	}
+
+	leftDataPoints := []DataPoint{}
+	rightDataPoints := []DataPoint{}
+	splitValue := computeSplitValue(dataPoints, splitFeature)
+	for _, dp := range dataPoints {
+		if dp.features[splitFeature] < splitValue {
+			leftDataPoints = append(leftDataPoints, dp)
+		} else {
+			rightDataPoints = append(rightDataPoints, dp)
+		}
+	}
+
+	left := buildTree(leftDataPoints, depth+1)
+	right := buildTree(rightDataPoints, depth+1)
+
+	return &node{
+		splitFeature: splitFeature,
+		splitValue:   splitValue,
+		left:         left,
+		right:        right,
+	}
+}
+
+func traverseTree(features []float64, n *node) int {
+	if n.label != -1 {
+		return n.label
+	}
+	if features[n.splitFeature] < n.splitValue {
+		return traverseTree(features, n.left)
+	} else {
+		return traverseTree(features, n.right)
+	}
+}
+
+func computeInfoGain(dataPoints []DataPoint, featureIndex int) float64 {
+	totalEntropy := computeEntropy(dataPoints)
+	leftDataPoints := []DataPoint{}
+	rightDataPoints := []DataPoint{}
+	splitValue := computeSplitValue(dataPoints, featureIndex)
+	for _, dp := range dataPoints {
+		if dp.features[featureIndex] < splitValue {
+			leftDataPoints = append(leftDataPoints, dp)
+		} else {
+			rightDataPoints = append(rightDataPoints, dp)
+		}
+	}
+	leftEntropy := computeEntropy(leftDataPoints)
+	rightEntropy := computeEntropy(rightDataPoints)
+	leftFraction := float64(len(leftDataPoints)) / float64(len(dataPoints))
+	rightFraction := float64(len(rightDataPoints)) / float64(len(dataPoints))
+	return totalEntropy - leftFraction*leftEntropy - rightFraction*rightEntropy
+}
+
+func computeEntropy(dataPoints []DataPoint) float64 {
+	labelCounts := make(map[int]int)
+	for _, dp := range dataPoints {
+labelCounts[dp.label]++
+}
+entropy := 0.0
+for _, count := range labelCounts {
+prob := float64(count) / float64(len(dataPoints))
+entropy -= prob * math.Log2(prob)
+}
+return entropy
+}
+
+func computeSplitValue(dataPoints []DataPoint, featureIndex int) float64 {
+sum := 0.0
+for _, dp := range dataPoints {
+sum += dp.features[featureIndex]
+}
+return sum / float64(len(dataPoints))
+}
