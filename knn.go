e373c60fc6cbcfb7d53e3520d243bcfdb401f2af
hamidreza ghahremani <97837767+hamidreza01@users.noreply.github.com>
2023-04-17 19:39:41 +0330

    Implement K-Nearest Neighbors algorithm
    This commit adds a new function, KNN, which implements the K-Nearest Neighbors algorithm. The KNN algorithm is a simple machine learning algorithm that can be used for classification problems. Given a set of data points with known labels, and a new data point without a label, the algorithm finds the K closest data points to the new point and assigns the most common label among those data points to the new point.
    
    The KNN function takes two inputs: K, the number of nearest neighbors to consider, and dataPoints, a slice of DataPoint structs, where each DataPoint struct contains a set of features and a label. The function returns a pointer to a KNN struct, which contains the input data and provides a classify function for making predictions on new data.
    
    The classify function takes a point with a slice of features as input, computes the Euclidean distance between the new point and each of the input data points, sorts the input data points by distance, selects the K nearest data points, and assigns the most common label among those data points to the new point.

---
+++ knn.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+    "fmt"
+    "math"
+    "sort"
+)
+
+type DataPoint struct {
+    Features []float64
+    Label    float64
+}
+
+type Distance struct {
+    Index int
+    Value float64
+}
+
+type KNN struct {
+    K          int
+    DataPoints []DataPoint
+}
+
+func NewKNN(k int, dataPoints []DataPoint) *KNN {
+    return &KNN{
+        K:          k,
+        DataPoints: dataPoints,
+    }
+}
+
+func (knn *KNN) distance(point1 []float64, point2 []float64) float64 {
+    sum := 0.0
+    for i := 0; i < len(point1); i++ {
+        sum += math.Pow(point1[i]-point2[i], 2)
+    }
+    return math.Sqrt(sum)
+}
+
+func (knn *KNN) classify(point []float64) float64 {
+    distances := make([]Distance, len(knn.DataPoints))
+    for i := 0; i < len(knn.DataPoints); i++ {
+        distances[i] = Distance{
+            Index: i,
+            Value: knn.distance(point, knn.DataPoints[i].Features),
+        }
+    }
+    sort.Slice(distances, func(i, j int) bool { return distances[i].Value < distances[j].Value })
+
+    count := make(map[float64]int)
+    for i := 0; i < knn.K; i++ {
+        count[knn.DataPoints[distances[i].Index].Label]++
+    }
+
+    maxCount := 0
+    maxLabel := 0.0
+    for label, c := range count {
+        if c > maxCount {
+            maxCount = c
+            maxLabel = label
+        }
+    }
+
+    return maxLabel
+}
